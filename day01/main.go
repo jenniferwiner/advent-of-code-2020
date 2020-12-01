838a9184f452e57436eb37a7ca7eea969fcf90d0
jenniferwiner <jeniwiner@gmail.com>
2020-12-01 10:20:52 -0700

    part 1

---
+++ day01/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := readFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	hashmap := map[int]bool{}
+	for _, val := range input {
+		hashmap[val] = true
+	}
+
+	for _, val := range input {
+		lookingFor := 2020 - val
+		_, ok := hashmap[lookingFor]
+		if ok {
+			fmt.Printf("Found them: %v, %v\n", val, lookingFor)
+			fmt.Printf("Multiply: %v\n", val * lookingFor)
+			break
+		}
+	}
+}
+
+func readFile(filename string) (nums []int, err error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(b), "\n")
+	// Assign cap to avoid resize on every append.
+	nums = make([]int, 0, len(lines))
+
+	for _, l := range lines {
+		// Empty line occurs at the end of the file when we use Split.
+		if len(l) == 0 {
+			continue
+		}
+		// Atoi better suits the job when we know exactly what we're dealing
+		// with. Scanf is the more general option.
+		n, err := strconv.Atoi(l)
+		if err != nil {
+			return nil, err
+		}
+		nums = append(nums, n)
+	}
+
+	return nums, nil
+}
