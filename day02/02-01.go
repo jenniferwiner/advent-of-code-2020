d2b904032d73efc14eb6933e370d13e8651ca234
jenniferwiner <jeniwiner@gmail.com>
2020-12-02 16:53:44 -0700

    day 2

---
+++ day02/02-01.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type minMaxPWDetails struct {
+	letter string
+	min int
+	max int
+}
+
+func main() {
+	input, err := buildMinMaxPWMap("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	validPwCount := 0
+	for pw, entry := range input {
+		count := strings.Count(pw, entry.letter)
+		if count >= entry.min && count <= entry.max {
+			validPwCount++
+		}
+	}
+	fmt.Printf("Valid Passwords: %v", validPwCount)
+}
+
+func buildMinMaxPWMap(filename string) (map[string]minMaxPWDetails, error) {
+	path, err := filepath.Abs(filename)
+	fmt.Println(path)
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(b), "\n")
+	entries := map[string]minMaxPWDetails{}
+	for _, l := range lines {
+		// Empty line occurs at the end of the file when we use Split.
+		if len(l) == 0 {
+			continue
+		}
+		// Atoi better suits the job when we know exactly what we're dealing
+		// with. Scanf is the more general option.
+		contents := strings.Split(l, " ")
+		numStr := contents[0]
+		nums := strings.Split(numStr, "-")
+		let := contents[1]
+		pw := contents[2]
+
+		min, err := strconv.Atoi(nums[0])
+		if err != nil {
+			return nil, err
+		}
+		max, err := strconv.Atoi(nums[1])
+		if err != nil {
+			return nil, err
+		}
+		entries[pw] = minMaxPWDetails{
+			letter: let[:1],
+			min:    min,
+			max:    max,
+		}
+	}
+
+	return entries, nil
+}
+
---
+++ day02/02-02.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type posPWDetails struct {
+	letter string
+	pos []int
+}
+
+func main() {
+	input, err := buildPosPWMap("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	validPwCount := 0
+	for pw, entry := range input {
+		count := 0
+		for p, v := range pw {
+			currLetter := string(v)
+			if entry.pos[0] == p && currLetter == entry.letter {
+				count++
+			}
+			if entry.pos[1] == p && currLetter == entry.letter {
+				count++
+			}
+		}
+		if count == 1 {
+			validPwCount++
+		}
+	}
+	fmt.Printf("Valid Passwords: %v", validPwCount)
+}
+
+func buildPosPWMap(filename string) (map[string]posPWDetails, error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(b), "\n")
+	entries := map[string]posPWDetails{}
+	for _, l := range lines {
+		// Empty line occurs at the end of the file when we use Split.
+		if len(l) == 0 {
+			continue
+		}
+		// Atoi better suits the job when we know exactly what we're dealing
+		// with. Scanf is the more general option.
+		contents := strings.Split(l, " ")
+		numStr := contents[0]
+		nums := strings.Split(numStr, "-")
+		let := contents[1]
+		pw := contents[2]
+
+		pos1, err := strconv.Atoi(nums[0])
+		if err != nil {
+			return nil, err
+		}
+		pos2, err := strconv.Atoi(nums[1])
+		if err != nil {
+			return nil, err
+		}
+		entries[pw] = posPWDetails{
+			letter: let[:1],
+			// normalize indexes
+			pos: []int{pos1 - 1, pos2 - 1},
+		}
+	}
+
+	return entries, nil
+}
+
